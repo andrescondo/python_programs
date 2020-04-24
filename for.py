6cb8000ce5754d4b292c439cf02c9753ac68bbaf
Andres Condo <1dkevinandres.b@gmail.com>
2020-04-24 12:38:47 -0500

    añadido problema con ciclo for

--- for.py
+++ for.py
@@ -1,5 +1,16 @@
+# 
+print('\nRealice un ciclo donde si el numero es excatamente divisible para 3 elevalo al cuadrado, y si el numero es 22 cierra el ciclo\n')
 for i in range(30):
-  if i % 3 != 0:
-    continue
-  else:
-    print(i**2)
+  if i % 3 == 0:
+    print(i ** 2)
+  elif i == 22:
+    break
+
+print('\n\n')
+
+
+for a in range(30):
+  if a % 3 == 0:
+    print(a)
+  elif a == 22:
+    break
