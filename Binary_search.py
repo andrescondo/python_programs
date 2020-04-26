edd1377a786adfed4e50781a3bdf6cab323f9dec
Andres Condo <1dkevinandres.b@gmail.com>
2020-04-26 13:46:34 -0500

    uso de busquedas binarias en el listas desordenadas, por medio de dos metodos

--- Binary_search.py
+++ Binary_search.py
@@ -16,11 +16,17 @@ def binary_search(numbers, number_to_find, low, high):
 
 
 if __name__ == '__main__':
-  numbers = [1,3,4,5,6,9,10,11,25,28,34,36,49,51]
+
+  numbers = [2,5,7,3,5,9,7,4,45,12,78,89,12,54,56,1,47,97,32,30]
+  # Formas de ordenar listas desordenadas
+  #numbers.sort() # Forma 1
+  numbers = sorted(numbers) # Forma 2
+  #----------------------------------------
 
   number_to_find = int(raw_input('Ingrese un numero: '))
 
   result = binary_search(numbers, number_to_find, 0 , len(numbers) - 1)
+
 
   if result is True:
     print('El numero si esta en la lista.')
