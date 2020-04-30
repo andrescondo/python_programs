b5bff0dd4d19d4217f8dba62b366849a3fe5048a
Andres Condo <1dkevinandres.b@gmail.com>
2020-04-30 11:33:29 -0500

    agregado gitignore, texto, y programa para el manejo de archivos(lectura de palabras repetidas

---
+++ search_in_file.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+
+def run():
+    word = 'hay'
+    counter = 0
+    with open('aleph.txt') as f:
+        for line in f :
+            counter += line.count(word)
+
+    print('{} se encontro {} veces en el texto'.format(word, counter))
+
+if __name__ == '__main__':
+    run()
