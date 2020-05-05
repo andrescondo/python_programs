da2356fea0e9cfa682b1454cc34019983dd77006
Andres Condo <1dkevinandres.b@gmail.com>
2020-05-05 13:37:10 -0500

    agenda de contactos implementado el uso de clases, para el ingreso de datos

--- contacts.py
+++ contacts.py
@@ -1,10 +1,33 @@
 
 
+class Contact:
+
+    #constructor (parametros(self, ))
+    def __init__(self, name, phone, email ):
+        #guion bajo sig. es privada
+        self._name = name
+        self._phone = phone
+        self._email = email
+
+
+class ContactBook:
+
+    def __init__(self):
+        self._contacts = []
+
+
+    def add(self, name, phone, email):
+        print('name: {}, phone: {}, email: {}'.format(name, phone, email))
+
+
+
 def run():
 
+    contact_book = ContactBook()
+
     while True:
-        command = str(raw_input('''
-            Que deseas hacer?
+        command = str(input('''
+            ¿Qué deseas hacer?
 
             [a]ñadir contacto
             [ac]tualizar contacto
@@ -16,7 +39,12 @@ def run():
             '''))
 
         if command == 'a':
-            print('añadir contacto')
+
+            name = str(input('Escriba el nombre del contacto: '))
+            phone = str(input('Escribe el tel del contacto: '))
+            email = str(input('Escribe el email del contacto: '))
+
+            contact_book.add(name, phone, email)
 
         elif command == 'ac':
             print('actualizar contacto')
@@ -38,4 +66,5 @@ def run():
 
 
 if __name__ == '__main__':
+    print('A G E N D A - D E - C O N T A C T O S ')
     run()
