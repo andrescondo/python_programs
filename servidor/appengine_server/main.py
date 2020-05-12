b6450fe35635a3c31099bf976577ed29d70386e5
Andres Condo <1dkevinandres.b@gmail.com>
2020-05-11 19:12:55 -0500

    servidor con google cloud corriendo

--- servidor/appengine_server/main.py
+++ servidor/appengine_server/main.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hola():
-    return '¡Hola, mundo!'
+    return render_template('index.html')
+    # la carpeta donde se manda, debe llamarse "templates", en caso de no poner el nombre correcto el programa no correra
 
 
 
