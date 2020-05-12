db31f4479184cb112407b803dbc6fbb82047b643
Andres Condo <1dkevinandres.b@gmail.com>
2020-05-11 21:04:45 -0500

    manejo de datos, añadidos en base de datos

--- servidor/appengine_server/main.py
+++ servidor/appengine_server/main.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask,render_template
+from flask import Flask,render_template, request
+from contact_model import Contact
 app = Flask(__name__)
 
 
-@app.route('/')
-def hola():
-    return render_template('index.html')
-    # la carpeta donde se manda, debe llamarse "templates", en caso de no poner el nombre correcto el programa no correra
+@app.route(r'/', methods=['GET'])
+def contact_book():
+    return render_template('contact_book.html')
 
+
+@app.route(r'/add', methods=['GET','POST'])
+def add_contact():
+
+    if request.form:
+        contact = Contact(name=request.form.get('name'),
+                          phone=request.form.get('phone'),
+                          email=request.form.get('email') )
+
+        contact.put()
+ 
+    return render_template('add_contacts.html')
 
 
 if __name__ == '__main__':
